9958d8413669aeaf54fc391d2e297b96793b881f
Mercedes <mercedesgmiller92@gmail.com>
2019-06-21 11:01:09 -0400

    hid the api key, deleted server folder, worked on redux store

--- store/posts.js
+++ store/posts.js
@@ -11,12 +11,65 @@ const defaultPosts = {
 const gotMyPosts = (posts) => ({type: GOT_MY_POSTS, posts})
 const gotAllPosts = (posts) => ({type: GOT_ALL_POSTS, posts})
 
-// THUNKS
-// get all posts
-// get posts by me
-// add post
-// mark as clean
-// delete post
+export const getAllPosts = () => {
+  return async dispatch => {
+    try {
+      const {data} = await axios.get('http://localhost:8080/api/posts')
+      dispatch(gotAllPosts(data))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+export const getMyPosts = (id) => {
+  return async dispatch => {
+    try {
+      const {data} =  await axios.get(`http://localhost:8080/api/posts/${id}`)
+      dispatch(gotMyPosts(data))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+export const addPost = (post, id) => {
+  return async dispatch => {
+    try {
+      await axios.post(`http://localhost:8080/api/posts`, post)
+      dispatch(getAllPosts())
+      dispatch(getMyPosts(id))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+export const markClean = (cleanImage, postId, userId) => {
+  return async dispatch => {
+    try {
+      await axios.put(`http://localhost:8080/api/posts/clean/${postId}`, cleanImage)
+      dispatch(getAllPosts())
+      if (userId) {
+        dispatch(getMyPosts(userId))
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+export const deletePost = (id, userId) => {
+  return async dispatch => {
+    try {
+      await axios.delete(`http://localhost:8080/api/posts/${id}`)
+      dispatch(getAllPosts())
+      dispatch(getMyPosts(userId))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
 
 export default function(state = defaultPosts, action) {
     switch (action.type) {
